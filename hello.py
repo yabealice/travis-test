9ef141d44eb459745c9b4071724d01b41e1ac0ea
alice <aliceliu0106@gmail.com>
2018-05-29 10:21:49 +0800

    add tensorflow

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 print("Hello world!")
+import tensorflow as tf
 #new line
+hello = tf.constant('Hello, TensorFlow!')  
+ 
+#Start tf session 
+sess = tf.Session() 
+ 
+
+print(sess.run(hello)) 
+
 
